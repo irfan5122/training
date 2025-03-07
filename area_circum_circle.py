3db5091ea643b85f6b122f143fb46a86f456c762
irfan5122 <161944240+irfan5122@users.noreply.github.com>
2025-03-07 14:50:38 +0530

    Create area_circum_circle.py

---
+++ area_circum_circle.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from math import pi
+r = float(input())
+c = 2*pi*r
+a = pi*r*r
+
+print(f"The circumference of the circle is: {c:.2f}")
+print(f"The area of the circle is: {a:.2f}")
