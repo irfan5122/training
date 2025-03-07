e1fa870fcd39385978600888b9690621004ba815
irfan5122 <161944240+irfan5122@users.noreply.github.com>
2025-03-07 15:01:08 +0530

    Create vowels_count.py

---
+++ vowels_count.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+vowels = ['a','e','i','o','u']
+usr = input().lower()
+count = 0
+for i in usr:
+    if i in vowels:
+        count+=1
+        
+if count == 0:
+    print("No vowels were found.")
+else:
+    print(f"Total number of vowels: {count}")
