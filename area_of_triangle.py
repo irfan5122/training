d1acc9dd5c4c6b02129e0d8847b9614dfe0d79a1
irfan5122 <161944240+irfan5122@users.noreply.github.com>
2025-03-07 12:23:29 +0530

    Create area_of_triangle.py

---
+++ area_of_triangle.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+
+a = float(input())
+b = float(input())
+c = float(input())
+if ((a+b)>c and (a+c)>b and (c+b)>a):
+    s = (a+b+c)/2
+    area = math.sqrt(s*(s-a)*(s-b)*(s-c)) 
+    print(f"The area of the triangle is: {area:.2f}")
+else:
+    print("Invalid Triangle")
